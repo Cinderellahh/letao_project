42432ba84a3f8fe791e9a4f89761e0da9c2177e0
qiaohuahua <qiaohh1992@163.com>
2018-06-09 17:36:55 +0800

    修改分类页面的头部,给分类页面的产品部分增加内部滚动功能

--- public/m/js/category.js
+++ public/m/js/category.js
@@ -3,6 +3,7 @@
     $(function(){
     //new一个对象
     var letao=new Letao();
+    letao.initScroll();
     letao.getLeftData();
     letao.getRightData();
     })
@@ -14,6 +15,18 @@
 
     //在构造函数的原型链中增加方法
     Letao.prototype={
+        initScroll:function (){
+            //内部滚动
+            mui('.mui-scroll-wrapper').scroll({
+              scrollY: true, //是否竖向滚动
+             scrollX: false, //是否横向滚动
+             startX: 0, //初始化时滚动至x
+             startY: 0, //初始化时滚动至y
+             indicators: true, //是否显示滚动条
+             deceleration:0.0006, //阻尼系数,系数越小滑动越灵敏
+             bounce: true //是否启用回弹
+            });
+          },
 
         //请求左边的数据
         getLeftData:function (){
